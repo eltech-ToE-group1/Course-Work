a5551798d6b611584ad83e11e662ea420fd97da0
DudkoKirill <fire.dragon.of.darkness@gmail.com>
2017-04-28 23:15:43 +0300

    Update sigproc.py

--- sigproc.py
+++ sigproc.py
@@ -24,3 +24,54 @@ class SignalCircuit(cir.Circuit):
             self.signal = signal
         # Время сигнала
         self.Tau = Tau
+
+    def H_t(self)
+        ABCD = circ.StateSpace(5, 'I')
+        HS = ss2tf(a[0], a[1], a[2], a[3])
+                H_S=ss2tf(ABCD[0],ABCD[1],ABCD[2],ABCD[3])
+        s=sp.Symbol('s')
+        t=sp.Symbol('t')
+        print(H_S)
+        HS0=''
+        HS1=''
+        f=0;
+        j=H_S[0][0].size-1;
+        for i in H_S[0][0]:
+            num=str(i)
+            if i:
+                if(j>1):
+                    num=num+'*s**'+str(j)
+                else:
+                    if (j>0):
+                        num=num+'*s'
+                if f:
+                    if (i<0):
+                        HS0=HS0+'-'
+                    else:
+                        HS0=HS0+'+'
+                else:
+                    f=1;
+                HS0=HS0+num
+            j=j-1
+        j=H_S[1].size-1
+        f=0
+        for i in H_S[1]:
+            num=str(i)
+            if i:
+                if(j>1):
+                    num=num+'*s**'+str(j)
+                else:
+                    if (j>0):
+                        num=num+'*s'
+                if f:
+                    if (i<0):
+                        HS1=HS1+'-'
+                    else:
+                        HS1=HS1+'+'
+                else:
+                    f=1;
+                HS1=HS1+num
+            j=j-1
+        HS='('+HS0+')'+'/('+HS1+')'
+        print (HS)
+        print(sp.inverse_laplace_transform(HS,s,t))
