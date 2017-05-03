3694cbad9129d968e49779d19c39b627b917191a
Denis Kozlov <ConstrayRUS@gmail.com>
2017-05-03 19:47:19 +0400

    it's just works

--- sigproc.py
+++ sigproc.py
@@ -1,26 +1,49 @@
 import circuit as cir
-from sympy.abc import  s, t
-from sympy import Heaviside, sin, cos, exp
-from numpy import pi
-from sympy.integrals import laplace_transform, inverse_laplace_transform
-from sympy.parsing.sympy_parser import parse_expr
-from scipy.fftpack import  fft, fftshift
+from sympy import Heaviside, sin, cos, symbols
+from scipy.fftpack import fft, fftshift
 from scipy.signal import ss2tf
 import matplotlib.pyplot as plt
 import copy
 import numpy as np
 
+# Расчёт значений обратного преобразования лапласа функции f_s, заданной матрично в точках t_z
+def talbot_inverse(f_s, t_z, M = 64):
+    k = np.arange(M)
+    delta = np.zeros(M, dtype=complex)
+    for i in k:
+        if i != 0:
+            delta[i] = 2*np.pi/5 * i * (np.tan(np.pi/M*i)**(-1)+1.j)
+    delta[0] = 2*M/5
+    gamma = np.zeros(M, dtype=complex)
+    for i in k:
+        if i != 0:
+            gamma[i] = (1 + 1.j*np.pi/M*i*(1+np.tan(np.pi/M*i)**(-2))-1.j*np.tan(np.pi/M*i)**(-1))*np.exp(delta[i])
+    gamma[0] = 0.5*np.exp(delta[0])
+    delta_mesh, t_mesh = np.meshgrid(delta, t_z)
+    gamma_mesh = np.meshgrid(gamma,t_z)[0]
+    points = delta_mesh/t_mesh
+    fun_res_ch = np.zeros(np.shape(points), dtype=complex)
+    fun_res_zn = np.zeros(np.shape(points), dtype=complex)
+    for i in range(len(f_s[0])):
+        fun_res_ch = fun_res_ch + (f_s[0][i])*points**(len(f_s[0]) - i)
+    for i in range(len(f_s[1])):
+        fun_res_zn = fun_res_zn + (f_s[1][i])*points**(len(f_s[1]) - i)
+    fun_res = fun_res_ch/fun_res_zn
+    sum_ar = np.real(gamma_mesh*fun_res)
+    sum_ar = np.sum(sum_ar, axis = 1)
+    ilt = 0.4/t_z * sum_ar
+    return ilt
 
 class SignalCircuit(cir.Circuit):
 
     def __init__(self, node_array, el_array, sigtype, A, Tau, signal = 0):
         cir.Circuit.__init__(self, node_array, el_array)
         if sigtype == 1:
-            self.signal = A*(sin(t*pi/Tau) - sin(t*pi/Tau)*Heaviside(t-Tau))
+            self.signal = A*(sin(t*np.pi/Tau) - sin(t*np.pi/Tau)*Heaviside(t-Tau))
         if sigtype == 2:
             self.signal = 2*A/Tau*(t-t*Heaviside(t - Tau/2))+(-2*A/Tau*t +2*A)*(Heaviside(t- Tau/2)-Heaviside(t - Tau))
         if sigtype == 3:
-            self.signal = A*(cos(t*pi/Tau) - cos(t*pi/Tau)*Heaviside(t-Tau))
+            self.signal = A*(cos(t*np.pi/Tau) - cos(t*np.pi/Tau)*Heaviside(t-Tau))
         if sigtype == 4:
             self.signal = A*(1 - 2*Heaviside(t-Tau/2) + Heaviside(t-Tau))
         # Сигнал, заданный пользователем
@@ -29,58 +52,16 @@ class SignalCircuit(cir.Circuit):
         # Время сигнала
         self.Tau = Tau
 
-    def H_t(self,num, type):
+    def H_s(self,num, type):
         ABCD = cir.Circuit.StateSpace(self, num, type)
-        H_Sold=ss2tf(ABCD[0],ABCD[1],ABCD[2],ABCD[3])
-        H_S = []
-        # округляем значения до второй значащей цифры
-        H_S.append(np.around(H_Sold[0], decimals = 4))
-        H_S.append(np.around(H_Sold[1], decimals = 4))
-        HS0=''
-        HS1=''
-        f=0
-        j=H_S[0][0].size-1
-        for i in H_S[0][0]:
-            num=str(i)
-            if i:
-                if(j>1):
-                    num=num+'*s**'+str(j)
-                else:
-                    if (j>0):
-                        num=num+'*s'
-                if f:
-                    if (i<0):
-                        HS0=HS0+'-'
-                    else:
-                        HS0=HS0+'+'
-                else:
-                    f=1
-                HS0=HS0+num
-            j=j-1
-        j=H_S[1].size-1
-        f=0
-        for i in H_S[1]:
-            num=str(i)
-            if i:
-                if(j>1):
-                    num=num+'*s**'+str(j)
-                else:
-                    if (j>0):
-                        num=num+'*s'
-                if f:
-                    if (i<0):
-                        HS1=HS1+'-'
-                    else:
-                        HS1=HS1+'+'
-                else:
-                    f=1
-                HS1=HS1+num
-            j=j-1
-        HS='('+HS0+')'+'/('+HS1+')'
-        HS = parse_expr(HS, evaluate=False)
-        HS1 = HS/s
-        # Возвращает h(t), h1(t)
-        return (inverse_laplace_transform(HS,s,t)), (inverse_laplace_transform(HS1,s,t))
+        HStp=ss2tf(ABCD[0],ABCD[1],ABCD[2],ABCD[3])
+        H1S_zn = np.zeros(len(HStp[1]) + 1)
+        for i in range(len(HStp[1])):
+            H1S_zn[i] = HStp[1][i]
+        H1S = HStp[0][0], H1S_zn
+        HS = HStp[0][0], HStp[1]
+        # Возвращает H(s), H1(s)
+        return HS, H1S
 
     # N - Количество точек
     def Fourier(self, N = 200):
@@ -134,46 +115,27 @@ class SignalCircuit(cir.Circuit):
     # Создаёт точки для графиков h(t), h1(t) f2(t) для элемента num и типа реакции type
     def hth1tGraph(self, num, type, N = 200, deltay = 0.0001):
         T = self.Tau / N
-        x = np.linspace(0.0, N * T, N)
-        hs = self.H_t(num,type)
+        x = np.linspace(deltay, N * T, N)
+        hs = self.H_s(num,type)
         h = hs[0]
         h1 = hs[1]
-        yh = []
-        for i in x:
-            if int(i) == i:
-                h = h.subs(Heaviside(t - int(i)), 1)
-            else:
-                h = h.subs(Heaviside(t - i), 1)
-            yh.append(h.subs(t, i))
-        yh1 = []
-        for i in x:
-            if int(i) == i:
-                h1 = h1.subs(Heaviside(t - int(i)), 1)
-            else:
-                h1 = h1.subs(Heaviside(t - i), 1)
-            yh1.append(h1.subs(t, i))
+        yh = talbot_inverse(h,x)
+        yh1 = talbot_inverse(h1,x)
         while abs(yh[len(yh) - 1] - yh[len(yh) - 2]) > deltay:
-            x = np.append(x, x[len(x) - 1] + T)
-            i = x[len(x) - 1]
-            if int(i) == i:
-                h = h.subs(Heaviside(t - int(i)), 1)
-                h1 = h1.subs(Heaviside(t - int(i)), 1)
-            else:
-                h = h.subs(Heaviside(t - i), 1)
-                h1 = h1.subs(Heaviside(t - i), 1)
-            yh.append(h.subs(t, i))
-            yh1.append(h1.subs(t, i))
+            temp = x[len(x) - 1] + T
+            x = np.append(x,temp)
+            yh = np.append(yh, talbot_inverse(h,temp))
+            yh1 = np.append(yh1, talbot_inverse(h1, temp))
         return x, yh, yh1
 
 
-
-
+t = symbols("t", positive=True)
 node_array = [cir.Node(0), cir.Node(1), cir.Node(2), cir.Node(3)]
-node_elem = [cir.Element(0, "I", 1, 1, None, 0, 1), cir.Element(1, "R", 2, None, None, 1, 0),
-             cir.Element(2, "L", 2, None, None, 1, 2), cir.Element(3, "R", 1, None, None, 2, 3),
-             cir.Element(4, "C", 4, None, None, 3, 0), cir.Element(5, "R", 0.5, None, None, 3, 0)]
-circ = SignalCircuit(node_array, node_elem, 3, 1, 2)
-hxy = circ.hth1tGraph(5,'I', 100, 0.00001)
+node_elem = [cir.Element(0, "U", 1, None, 1, 0, 1), cir.Element(1, "C", 4, None, None, 1, 2),
+             cir.Element(2, "R", 1, None, None, 2, 1), cir.Element(3, "C", 1, None, None, 2, 3),
+             cir.Element(4, "R", 0.5, None, None, 3, 0), cir.Element(5, "R", 0.5, None, None, 3, 0)]
+circ = SignalCircuit(node_array, node_elem, 3, 1, 5)
+hxy = circ.hth1tGraph(5,'U', 100)
 fftxy = circ.Fourier()
 sigxy = circ.SignalGraph(hxy[0])
 f2x = sigxy[0]
